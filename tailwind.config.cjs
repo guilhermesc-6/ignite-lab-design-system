54cd50f55b4955e5c8cf2719ce1c5da433c8d075
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2022-10-13 17:01:44 -0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
+    colors: {
+      transparent: "transparent",
+
+      black: "#00000",
+      white: "#ffffff",
+
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7c7c8a",
+        200: "#c4c4cc",
+        100: "#e1e1e6",
+      },
+
+      cyan: {
+        500: "#81d8f7",
+        300: "#98e1fb",
+      },
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+  },
+  plugins: [],
+};
